5002241c4da730db5288744a080ee99761ad3f45
Daniel Sanandaj <daniel.sanandaj@gmail.com>
2020-05-01 20:59:53 -0700

    added wpm and backspace does not count errors

--- Project3/script.js
+++ Project3/script.js
@@ -1,12 +1,25 @@
-console.log('here');
-
 const testWrapper = document.querySelector(".test-wrapper");
 const testArea = document.querySelector("#test-area");
-const originText = document.querySelector("#origin-text p").innerHTML;
+//const originText = document.querySelector("#origin-text p").innerHTML;
+const setOriginText = document.querySelector("#origin-text p");
 const resetButton = document.querySelector("#reset");
+const newButton = document.querySelector("#new");
 const theTimer = document.querySelector(".timer");
 const numErrors = document.querySelector("#errors");
 const wpmW = document.querySelector("#wpm");
+
+//array of texts
+var strings = [
+"Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.", 
+"This is test text for the typing program, see how fast you can type this sentence.",
+"The quick brown fox jumps over the lazy dog",
+"The text to test."
+];
+
+//array for word count
+var wordCount = [
+138, 16, 9, 4
+];
 
 //check if clock is running
 var running = false;
@@ -18,7 +31,19 @@ var wpm = 0;
 var old = 0;
 //check for error
 var error = false;
+//number for picking text
+var num = -1;
+//formatted time
+var minutes = '', seconds = '', mili = '';
 
+//pick random string
+function randomString() {
+    num = Math.floor(Math.random() * strings.length);
+
+    setOriginText.innerHTML = strings[num];
+    originText = document.querySelector("#origin-text p").innerHTML;
+}
+randomString();
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function zeroNum(time) {
@@ -28,15 +53,18 @@ function zeroNum(time) {
 // Run a standard minute/second/hundredths timer:
 function Timer() {
     
+    //variables
     var time = 0;
     var interval;
     var offset;
 
+    //update the current time every 10ms
     function update(){
-        time+= change();
-        var formatTime = format(time);
+        time += change();
+        formatTime = format(time);
         theTimer.textContent = formatTime;
     };
+    //find how much time passed
     function change(){
         var current = Date.now();
         var timePass = current - offset;
@@ -44,12 +72,14 @@ function Timer() {
         return timePass;
     };
 
+    //format the time
     function format(unformattedTime){
         var time = new Date(unformattedTime);
-        var minutes = time.getMinutes().toString();
-        var seconds = time.getSeconds().toString();
-        var mili = time.getMilliseconds().toString()    ;
+        minutes = time.getMinutes().toString();
+        seconds = time.getSeconds().toString();
+        mili = time.getMilliseconds().toString();
 
+        //add zeroes if necessary
         if(minutes.length < 2)
             minutes = zeroNum(minutes);
         if(seconds.length < 2)
@@ -60,16 +90,19 @@ function Timer() {
         return minutes + ':' + seconds + ':' + mili;
     };
 
+    //start the clock
     this.start = function(){
-            interval = setInterval(update, 10);
-            offset = Date.now();
-            running = true;
+        interval = setInterval(update, 10);
+        offset = Date.now();
+        running = true;
     };
+    //stop the clock
     this.stop = function(){
-            clearInterval(interval);
-            interval = null;
-            running = false;
+        clearInterval(interval);
+        interval = null;
+        running = false;
     };
+    //reset the clock
     this.reset = function(){
         time = 0;
         theTimer.textContent = "00:00:000";
@@ -79,24 +112,45 @@ function Timer() {
 var watch = new Timer();
 
 // Match the text entered with the provided text on the page:
-function matchedText(text){
-    console.log('testing');
+function matchedText(text, key){
+    //if the text is equal stop the click and disable the text box and make the border green
     if(text == originText) {
+        var time = getMin();
         watch.stop();
         testWrapper.style.border = "10px solid green";
         testArea.setAttribute("disabled", "disabled");
         numErrors.textContent = 'Errors: ' + errors;
+        wpm = wordCount[num] / time;
+        wpmW.textContent = 'Words per minute: '+ Math.trunc(wpm);
     }
-    else if(originText.charAt(text.length-1) != text.charAt(text.length-1)) {
-        if(text.length > old) {
+    //if there is an error check if the user went back to the start of the error if he didnt and made an error count that doesnt count backspaces
+    else if(error){
+        if(old == text.length) {
+            error = false;
+            testWrapper.style.border = "10px solid blue";
+        }
+        else {
+            if(originText.charAt(text.length-1) != text.charAt(text.length-1) && key.keyCode != 8)
+                errors++;
             testWrapper.style.border = "10px solid red";
-            console.log('error');
-            error = true;
-            errors++;
         }
     }
-    else {
+    //if there was an error mark error and count and make border red
+    else if(originText.charAt(text.length-1) != text.charAt(text.length-1)) {
+        error = true;
+        testWrapper.style.border = "10px solid red";
+        if(key.keyCode != 8) {
+               errors++;
+        }
+    }
+    //if there was no error make the border blue
+    else if(originText.charAt(text.length-1) == text.charAt(text.length-1)) {
+        error = false;
+        old = text.length;
         testWrapper.style.border = "10px solid blue";
+    }
+    //no error mark the lested no error point
+    else if (!error){
         old = text.length;
     }
 }
@@ -106,7 +160,7 @@ function startTimer() {
 }
 
 // Reset everything:
-function reset() {
+function reset(a) {
     watch.stop();
     watch.reset();
     testArea.value = "";
@@ -116,19 +170,21 @@ function reset() {
     errors = 0;
     wpmW.textContent = "";
     wpm = 0;
+    if(a == 1)
+        randomString();
 }
 
 // Event listeners for keyboard input and the reset button:
 //check for typing
-testArea.addEventListener('keyup', function(){
+testArea.addEventListener('keyup', function(a){
     //if timer not running turn on the timer
     if(!running && testArea.value.length > 0){
         startTimer();
     }
 
     //check if the text is equal
-    matchedText(testArea.value);
-    console.log('typing');
+    matchedText(testArea.value, a);
+    //console.log('typing');
 });
 
 //reset button
@@ -136,3 +192,11 @@ resetButton.addEventListener('click', function(){
     reset();
 });
 
+newButton.addEventListener('click', function(){
+    reset(1);
+});
+
+//calculate minutes for wpm
+function getMin() {
+    return parseInt(minutes) + (parseInt(seconds)/60) + (parseInt(mili)/60000);
+}
